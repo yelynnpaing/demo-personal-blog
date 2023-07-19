64a81839ef8365aadbdf60aa0ea7196c0c438b4a
yelynnpaing <yelynnpaing.mm@gmail.com>
2023-07-19 22:32:47 +0630

    first

---
+++ front/js/script.js
@@ -0,0 +1,10 @@
+window.onscroll = function() { myFun() };
+let navbar = document.getElementById('navbar');
+let topPx = navbar.offsetTop;
+function myFun() {
+    if(window.pageYOffset >= topPx) {
+        navbar.classList.add("fixMe");
+    }else{
+        navbar.classList.remove("fixMe");
+    }
+};
